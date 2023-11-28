d04f7190c2975788f7c02f85aed122635313ed8e
Dan LaManna <dan.lamanna@kitware.com>
2023-11-28 12:59:24 -0500

    wip

--- webhook-handler/analytics/settings.py
+++ webhook-handler/analytics/settings.py
@@ -45,3 +45,13 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Custom settings
+
+OPENSEARCH_ENDPOINT = os.environ["OPENSEARCH_ENDPOINT"]
+OPENSEARCH_USERNAME = os.environ["OPENSEARCH_USERNAME"]
+OPENSEARCH_PASSWORD = os.environ["OPENSEARCH_PASSWORD"]
+
+GITLAB_ENDPOINT: str = os.environ["GITLAB_ENDPOINT"]
+GITLAB_TOKEN: str = os.environ["GITLAB_TOKEN"]
--- webhook-handler/analytics/views.py
+++ webhook-handler/analytics/views.py
@@ -1,6 +1,16 @@
+from typing import Any
 from django.http import HttpRequest, HttpResponse
+import json
+
+from analytics.job_log_uploader import upload_job_log
 
 
 def webhook_handler(request: HttpRequest) -> HttpResponse:
-    print(request.body)
-    return HttpResponse("OK")
+    job_input_data: dict[str, Any] = json.loads(request.body)
+
+    if job_input_data.get("object_kind") != "build":
+        raise ValueError("Not a build event")
+
+    upload_job_log(job_input_data)
+
+    return HttpResponse("OK", status=202)
